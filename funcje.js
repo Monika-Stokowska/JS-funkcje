a843a38bb9bc91cee7ea6e04f54b4967518a0003
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2017-10-11 22:08:27 +0200

    Dodanie pliku o funkcjach

---
+++ funcje.js
@@ -0,0 +1,12 @@
+function getTriangleArea(a, h) {
+  if (a>0 && h>0) {
+  return a*h/2;
+  } else {
+  return 'Nieprawidłowe dane';
+  }
+  }
+  console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(-10, 35);
+var triangle3Area = getTriangleArea(1, 18);
