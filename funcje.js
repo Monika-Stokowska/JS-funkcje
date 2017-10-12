28e5dd7047f8ea1259020eee16a0e2869fbb1a6d
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2017-10-12 10:59:00 +0200

    Add files via upload

--- funcje.js
+++ funcje.js
@@ -1,10 +1,10 @@
 function getTriangleArea(a, h) {
-  if (a>0 && h>0) {
-  return a*h/2;
-  } else {
-  return 'Nieprawidłowe dane';
-  }
-  }
+    if (a>0 && h>0) {
+      return a*h/2;
+    } else {
+      return 'Nieprawidłowe dane';
+    }
+ }
   console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
